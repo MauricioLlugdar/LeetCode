0146e8c976e521fa0693aee2c855f29ffc5e403e
MauricioLlugdar <67161167+MauricioLlugdar@users.noreply.github.com>
2025-06-26 01:29:34 -0300

    Time: 13 ms (41.35%), Space: 45.1 MB (29.03%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        char[] cleanString = s.trim().replaceAll("[^a-zA-Z0-9]", "").toLowerCase().toCharArray();
-        int len = cleanString.length;
+        String cleanString = s.trim().replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int len = cleanString.length();
         int backwards = len-1;
         int forwards = 0;
         boolean res = true;
         while (backwards>forwards){
-            res = res && cleanString[forwards] == cleanString[backwards];
+            res = res && cleanString.charAt(forwards) == cleanString.charAt(backwards);
             backwards--;
             forwards++;
         }
