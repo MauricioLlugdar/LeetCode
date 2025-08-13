ab1f63faff9b8ef37d2078471ff9b27a0f393889
MauricioLlugdar <67161167+MauricioLlugdar@users.noreply.github.com>
2025-08-13 09:47:00 -0300

    Time: 816 ms (5.02%), Space: 47.6 MB (5.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,6 +4,9 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j < nums.length; j++) {
                 sums.put(nums[i]+nums[j], new int[]{i,j});
+                if(nums[i]+nums[j] ==target){
+                    return sums.get(target);
+                };
             }
         }
         return sums.get(target);
